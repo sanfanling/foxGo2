b7ef6a75918b7db3390ebc52d27bf73f62086b7a
sanfanling <xujia19@outlook.com>
2022-02-05 14:33:18 +0800

    realize steps control

--- mainWindow.py
+++ mainWindow.py
@@ -19,6 +19,19 @@ class mainWindow(baseWindow):
         
         self.newGame.triggered.connect(self.startFreeMode)
         self.fileOpen.triggered.connect(self.fileOpen_)
+        
+        self.toStartAction.triggered.connect(self.toStartAction_)
+        self.controlDock.controlWidget.toStartButton.clicked.connect(self.toStartAction_)
+        self.fastPrevAction.triggered.connect(self.fastPrevAction_)
+        self.controlDock.controlWidget.fastPrevButton.clicked.connect(self.fastPrevAction_)
+        self.prevAction.triggered.connect(self.prevAction_)
+        self.controlDock.controlWidget.prevButton.clicked.connect(self.prevAction_)
+        self.nextAction.triggered.connect(self.nextAction_)
+        self.controlDock.controlWidget.nextButton.clicked.connect(self.nextAction_)
+        self.fastNextAction.triggered.connect(self.fastNextAction_)
+        self.controlDock.controlWidget.fastNextButton.clicked.connect(self.fastNextAction_)
+        self.toEndAction.triggered.connect(self.toEndAction_)
+        self.controlDock.controlWidget.toEndButton.clicked.connect(self.toEndAction_)
     
     def startFreeMode(self):
         self.mode = "free"
@@ -28,7 +41,7 @@ class mainWindow(baseWindow):
         self.stepPoint = 0
         self.breakPoint = 0
         self.displayGameInfo()
-        self.reviewMove()
+        self.moveOnBoard()
     
     def startReviewMode(self, sgf):
         self.sgfData.init()
@@ -45,10 +58,57 @@ class mainWindow(baseWindow):
             self.thisGame.getHaSteps(self.sgfData.haList)
             self.resignAction.setEnabled(False)
             self.stepPoint = len(self.sgfData.stepsList)
+            self.breakPoint = 0
             self.controlDock.controlWidget.stepsCount.setRange(0, self.stepPoint)
             self.controlDock.controlWidget.stepsSlider.setRange(0, self.stepPoint)
             self.displayGameInfo()
-            self.reviewMove()
+            self.showStepsCount()
+            self.moveOnBoard()
+    
+    def toStartAction_(self):
+        self.stepPoint = self.breakPoint
+        self.showStepsCount()
+        self.moveOnBoard()
+    
+    def fastPrevAction_(self):
+        if self.stepPoint - 10 >= self.breakPoint:
+               self.stepPoint -= 10
+        else:
+            self.stepPoint = self.breakPoint
+        self.showStepsCount()
+        self.moveOnBoard()
+    
+    def prevAction_(self):
+        if self.stepPoint - 1 >= self.breakPoint:
+            self.stepPoint -= 1
+            self.showStepsCount()
+            self.moveOnBoard()
+    
+    def nextAction_(self):
+        if self.stepPoint + 1 <= len(self.sgfData.stepsList):
+            self.stepPoint += 1
+            self.showStepsCount()
+            self.moveOnBoard()
+
+    def fastNextAction_(self):
+        if self.stepPoint + 10 < len(self.sgfData.stepsList):
+            self.stepPoint += 10
+        else:
+            self.stepPoint = len(self.sgfData.stepsList)
+        self.showStepsCount()
+        self.moveOnBoard()
+    
+    def toEndAction_(self):
+        self.stepPoint = len(self.sgfData.stepsList)
+        self.showStepsCount()
+        self.moveOnBoard()
+    
+    def showStepsCount(self, setMaximum = False):
+        if setMaximum:
+            self.controlDock.controlWidget.stepsCount.setMaximum(self.stepPoint - self.breakPoint)
+            self.controlDock.controlWidget.stepsSlider.setMaximum(self.stepPoint - self.breakPoint)
+        self.controlDock.controlWidget.stepsCount.setValue(self.stepPoint - self.breakPoint)
+        self.controlDock.controlWidget.stepsSlider.setValue(self.stepPoint - self.breakPoint)
     
     def displayGameInfo(self):
         self.infoDock.infoDisplay.gameLabel.setText(self.sgfData.title)
@@ -61,9 +121,11 @@ class mainWindow(baseWindow):
         self.infoDock.infoDisplay.resultValue.setText(self.sgfData.result)
         self.infoDock.infoDisplay.timeLimitValue.setText(self.sgfData.timeLimit)
     
-    def reviewMove(self):
+    def moveOnBoard(self):
+        self.thisGame.stepsGoDict.clear()
         self.thisGame.stepsGoDict.update(self.thisGame.haDict)
-        for i, c, x, y in self.sgfData.stepsList:
+        tmpList = self.sgfData.stepsList[:self.stepPoint]
+        for i, c, x, y in tmpList:
             self.thisGame.stepNum = i
             self.thisGame.goColor = c
             self.thisGame.x = x
