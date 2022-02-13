e94f2c2c760c3159417e5d0213d988ee182008d3
sanfanling <xujia19@outlook.com>
2022-02-13 15:23:32 +0800

    update foxGo2 file for changing ini and lock file location

--- foxGo2.py
+++ foxGo2.py
@@ -9,17 +9,9 @@ import sys, os
 
 
 if __name__ == "__main__":
-    path1 = os.path.expanduser("~/.foxGo2")
-    if not os.path.exists(path1):
-        os.mkdir(path1)
-        
-    path2 = os.path.expanduser("~/.foxGo2/sgf")
-    if not os.path.exists(path2):
-        os.mkdir(path2)
     
-    lockFile = os.path.join(path1, "lock")
     app = QApplication(sys.argv)
-    if os.path.exists(lockFile):
+    if os.path.exists("lock"):
         QMessageBox.information(None, "Notice", "Only one foxGo2 instance allowed running at once!")
         sys.exit()
     else:
